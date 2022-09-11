8b660f5cf82a2fc2f5df33678d526ea21cba694f
Maksimov, Filipp <filipp.maksimov@team.wrike.com>
2022-09-11 14:28:39 +0200

    добавил превью

--- app/src/main/java/com/example/exchangeratestestapppublic/ui/BottomBar.kt
+++ app/src/main/java/com/example/exchangeratestestapppublic/ui/BottomBar.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.exchangeratestestapppublic.R
 
@@ -73,4 +74,10 @@ fun BottomBar(viewModel: ExchangeViewModel?, state: MainScreenState) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun BottomBarPreview() {
+    BottomBar(null, MainScreenState())
 }
