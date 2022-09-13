d3f0abcb8827a2e8cb8e7623d596328ea10133df
Andrey Marin <amarin@idscan.net>
2022-09-13 17:02:27 +0500

    delete double room initialization

--- app/src/main/java/com/example/exchangeratestestapppublic/App.kt
+++ app/src/main/java/com/example/exchangeratestestapppublic/App.kt
@@ -1,13 +1,9 @@
 package com.example.exchangeratestestapppublic
 
 import android.app.Application
-import com.example.exchangeratestestapppublic.db.Database
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Database.init(this)
-    }
+
 }
