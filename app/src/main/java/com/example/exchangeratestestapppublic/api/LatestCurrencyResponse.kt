fd70131d1da508450cea7a1f1a82d8d15d861d03
MarAndr <anmarin86@gmail.com>
2022-09-17 14:13:13 +0500

    delete default values in LatestCurrencyResponse.kt

--- app/src/main/java/com/example/exchangeratestestapppublic/api/LatestCurrencyResponse.kt
+++ app/src/main/java/com/example/exchangeratestestapppublic/api/LatestCurrencyResponse.kt
@@ -3,11 +3,11 @@ package com.example.exchangeratestestapppublic.api
 import java.util.*
 
 data class LatestCurrencyResponse(
-    val id: Long = 0,
-    val success: Boolean? = null,
-    val timestamp: Long? = null,
-    val base: String? = null,
-    val date: Date? = null,
-    val rates: Map<String, Double>? = null
+    val id: Long,
+    val success: Boolean?,
+    val timestamp: Long?,
+    val base: String?,
+    val date: Date?,
+    val rates: Map<String, Double>?
 )
 
